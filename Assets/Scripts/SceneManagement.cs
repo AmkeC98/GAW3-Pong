3769de0d6bbfd2e95df0ef8e6a92d8c47c13d320
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-04-01 11:28:29 +0200

    Adds scenemanagement, adds button-animation, adds instruction-scene

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void ShowInstructions()
+    {
+        Debug.Log("SHOW INSTRUCTIONS");
+        SceneManager.LoadScene("InstructionsScene");
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("START GAME");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+}

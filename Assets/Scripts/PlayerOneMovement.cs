37de19927c3d355a60aab926e70eb8caf1de325d
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-04-03 15:45:42 +0200

    Adds basic game-setup (walls + paddles + ball + dotted line) and adds paddle movement

---
+++ Assets/Scripts/PlayerOneMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneMovement : MonoBehaviour
+{
+    public GameObject paddle;
+    public float speed = 2.0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            paddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            paddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            paddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerTwoMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoMovement : MonoBehaviour
+{
+    public GameObject paddle;
+    public float speed = 2.0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            paddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            paddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            paddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+}
